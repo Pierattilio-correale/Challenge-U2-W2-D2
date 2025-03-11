013a14b626c8c7238bc422c972e5cc578fbb6f85
Pierattilio-correale <pierattiliocorreale@yahoo.it>
2025-03-11 19:06:16 +0100

    adds code

---
+++ scriptttt.js
@@ -0,0 +1,3 @@
+const autodistruzione = document.getElementById("autodistruzioneee");
+const cardsss = document.querySelectorAll(".card");
+autodistruzione.addEventListener("click", function () {});
